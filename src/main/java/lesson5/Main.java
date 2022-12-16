4ebe7b6117eed7e31ca59fd34752f5c01612c98f
atyuleneva <anastasiamhln@gmail.com>
2022-12-16 16:05:30 +0300

    lesson5

---
+++ src/main/java/lesson5/Main.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        AppData appData = new AppData();
+        appData.load("hw5.csv");
+
+        System.out.println(Arrays.toString(appData.getHeader()));
+        System.out.println(Arrays.deepToString(appData.getData()));
+
+        appData.save("hw5-1.csv");
+    }
+}
